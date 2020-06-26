d42256a8bc08af929ff2e30a28eb2014b4e94706
Mark Boyd <mark.boyd1414@gmail.com>
2020-06-25 21:01:28 -0400

    commiting lab 5

---
+++ models/superhero.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+// You need to create a new schema and assign it the following
+// constant
+const SuperheroSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  alias: {
+    type: String,
+    required: true
+  }
+}, {
+  timestamps: true
+});
+
+SuperheroSchema.virtual('synopsis')
+.get(function () {
+    const post = this.content;
+    return post
+  });
+
+module.exports = mongoose.model('Superhero', SuperheroSchema);
